42d53d27355847964cfc3ddce9bc3f47e482488d
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-12-08 11:36:45 +0530

    Updated the Book Mark Page

--- src/pages/Bookmarks/Bookmarks.tsx
+++ src/pages/Bookmarks/Bookmarks.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Bookmarks = () => {
   return (
-    <div className='page'>
-      <h2 className='pageTitle'>Welcome to Bookmarks</h2>
+    <div className="page">
+      <h2 className="pageTitle">Welcome to Bookmarks</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Bookmarks
+export default Bookmarks;
